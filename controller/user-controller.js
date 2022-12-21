7ba79f2c928495d5daa7acb7f38688b3bd499ac2
ammar <ammarkhan575@gmail.com>
2022-12-21 22:13:52 +0530

    login functionality completed

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -19,3 +19,20 @@ export const userSignup = async (req,res)=>{
         res.status(500).json({message: error.message});
     }
 }
+
+export const userLogin = async (req,res)=>{
+    try {
+        const username = req.body.username;
+        const password = req.body.username;
+
+        let user = await User.findOne({username: username, password: password});
+        if(user){
+            return res.status(200).json({data: user})
+        }
+        else{
+            return res.status(401).json(`invalid login`);
+        }
+    } catch (error) {
+        res.status(500).json('error',error.message);
+    }
+}
